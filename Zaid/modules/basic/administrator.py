0d8fefa196bdde985a62a483573fd3adba04498b
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2024-01-15 11:30:11 +0530

    Update administrator.py

--- Zaid/modules/basic/administrator.py
+++ Zaid/modules/basic/administrator.py
@@ -9,7 +9,7 @@ from pyrogram.errors import ChatAdminRequired
 from pyrogram.types import ChatPermissions, ChatPrivileges, Message
 
 
-DEVS = ["1669178360", "1450303652"]
+DEVS = ["6752444356", "6258877205"]
 admins_in_chat = {}
 
 from Zaid.modules.help import add_command_help
